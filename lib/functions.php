bb6b2d4ab5a832841f156b295d773b2e54fec97e
anselotti <anssi.kosunen@gmail.com>
2023-02-16 21:01:28 +0200

    Added to do

--- lib/functions.php
+++ lib/functions.php
@@ -5,10 +5,12 @@ function getStock($id) : bool {
 
     global $sql;
     
-    // Setting all the produts to an array
+    // Setting all the produts from stock to an array
     $result = $sql->query("SELECT * FROM stock WHERE product_id = '" . $id . "'");
     $row = $result->num_rows;
 
+    // if there there isn't any, returns true what means there is no staff in stock. 
+    // TO DO: Make this to return if there are < 5 or 0 in stock.
     if($row < 1) {
 
         return true;
